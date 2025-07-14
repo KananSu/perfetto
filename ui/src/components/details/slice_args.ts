b5696235e734ee11019ba563f40e7b8ef28ff281
KingAlen <19606165+KingAlen@users.noreply.github.com>
2025-07-14 19:59:18 +0800

    [BugFix] Hide "Visualize argument values" entry for "instance_id" arg key
    Summary of the change:
    
    The 'Focus LynxView' plugin introduces an 'instance_id' table, where every trace event with an ancestor containing the 'instance_id' arg key is also assigned this key and inserted into the table. This can result in a large number of trace events with the 'instance_id' arg key.
    Consequently, the "Visualize argument values" feature for the 'instance_id' arg key can become very costly due to extensive searches in the `ancestor_slice($id)` table. To optimize this, we now use the 'Focus LynxView' feature to replace "Visualize argument values" for this specific key, and have therefore hidden the "Visualize argument values" entry for the 'instance_id' arg key.

--- ui/src/components/details/slice_args.ts
+++ ui/src/components/details/slice_args.ts
@@ -34,6 +34,11 @@ export function renderSliceArguments(trace: Trace, args: ArgsDict): m.Children {
     args,
     (key, value) => {
       const displayValue = value === null ? 'NULL' : String(value);
+      const canFocusLynxView =
+        lynxPerfGlobals.state.lynxviewInstances.length > 0 &&
+        (key === 'debug.instance_id' || key === 'args.instance_id') &&
+        value != null &&
+        String(value).length > 0;
       const menuItems: m.Children[] = [
         m(MenuItem, {
           label: 'Find slices with same arg value',
@@ -61,20 +66,19 @@ export function renderSliceArguments(trace: Trace, args: ArgsDict): m.Children {
             });
           },
         }),
-        m(MenuItem, {
-          label: 'Visualize argument values',
-          icon: 'query_stats',
-          onclick: () => {
-            extensions.addVisualizedArgTracks(trace, key);
-          },
-        }),
       ];
-      if (
-        lynxPerfGlobals.state.lynxviewInstances.length > 0 &&
-        (key === 'debug.instance_id' || key === 'args.instance_id') &&
-        value != null &&
-        String(value).length > 0
-      ) {
+      if (!canFocusLynxView) {
+        menuItems.push(
+          m(MenuItem, {
+            label: 'Visualize argument values',
+            icon: 'query_stats',
+            onclick: () => {
+              extensions.addVisualizedArgTracks(trace, key);
+            },
+          }),
+        );
+      }
+      if (canFocusLynxView) {
         menuItems.push(
           m(MenuItem, {
             label: 'Focus LynxView',
