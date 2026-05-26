KingAlen/lynx-trace

[Optimize] UI improvements for NativeModule track
Summary of changes:
- Display invoke chain of NativeModule using three main trace events:
  NativeModule::Invoke, NativeModule::Callback, NativeModule::PlatformCallbackStart
- On clicking a NativeModule item in the track, show related flow events
- Keep NativeModule item content always centered within its cell
- Add diagonal slash lines to indicate areas not executed on the Background Thread

ui/src/lynx_perf/lynx_perf_globals.ts
@@ -54,6 +54,12 @@ class LynxPerfGlobals {
     });
   }
 
+  setNonTimingNativeModuleTraces(optimized: boolean) {
+    this._store.edit((draft) => {
+      draft.nonTimingNativeModuleTraces = optimized;
+    });
+  }
+
   updateVitalTimestampLine(timestamp: VitalTimestampLine[]) {
     this._store.edit((draft) => {
       draft.vitalTimestampLine = timestamp;

KananSu/perfetto

ui/src/lynx_perf/lynx_perf_globals.ts
@@ -54,6 +54,12 @@ class LynxPerfGlobals {
     });
   }
 
+  setNonTimingNativeModuleTraces(nonTimingNativeModuleTraces: boolean) {
+    this._store.edit((draft) => {
+      draft.nonTimingNativeModuleTraces = nonTimingNativeModuleTraces;
+    });
+  }
+
   updateVitalTimestampLine(timestamp: VitalTimestampLine[]) {
     this._store.edit((draft) => {
       draft.vitalTimestampLine = timestamp;